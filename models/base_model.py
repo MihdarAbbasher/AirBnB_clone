211c8f3ccb1e701c2ba59323f28e0a6964b11975
MihdarAbbasher <mihdar.ph@gmail.com>
2023-11-13 18:08:18 +0200

    rm circle import

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 """ defines all common attributes/methods for other classes """
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -21,7 +21,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """overwrite str method"""
